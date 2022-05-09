debfde8983f706804e5a70b6ccd2da25c1b26a7e
R_Shehu <rrustem.shehu@gmail.com>
2022-05-09 21:23:43 +0200

    Implement IdentityUser

---
+++ Idea.Domain/Data/IdeaDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Idea.Domain.Data
+{
+    public class IdeaDbContext : IdentityDbContext
+    {
+            public IdeaDbContext(DbContextOptions<IdeaDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
